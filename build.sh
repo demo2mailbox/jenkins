939366e4c35ccb2fb824158eef27c76c35f2af3c
chetangehlot <cpgehlot4@gmail.com>
2018-04-26 17:08:50 +0530

    modified

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
     docker info
     pwd
-    docker build -t cpg47b/cpg47b:${BUILD_NUMBER} .
-    docker images
+    docker build -t cpg47b/jenkins-new:${BUILD_NUMBER} .
+#    docker images
+    docker login -u cpg47b -p chhavi0709
+    docker push cpg47b/jenkins-new:${BUILD_NUMBER} 
     docker rm -f chetan
-    docker run -d --name chetan -p 8721:80 cpg47b/cpg47b:${BUILD_NUMBER}
+    docker run -d --name chetan -p 8721:80 cpg47b/jenkins-new:${BUILD_NUMBER}
