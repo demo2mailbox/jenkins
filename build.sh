81623e09e2affbc5ca819e116a83f98d817cfbb1
demo2mailbox <38717337+demo2mailbox@users.noreply.github.com>
2018-04-27 12:38:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@
     docker login -u cpg47b -p chhavi0709
     docker push cpg47b/jenkins-new:${BUILD_NUMBER} 
     docker rm -f chetan1
-    docker run -d --name chetan1 -p 8722:80 cpg47b/jenkins-new:${BUILD_NUMBER}
+    docker run -d --name chetan1 -p 8702:80 cpg47b/jenkins-new:${BUILD_NUMBER}
